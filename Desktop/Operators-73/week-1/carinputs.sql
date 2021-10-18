4744b413bd6334d7a51558cc6abd80a7fc144818
Joseph Fallon <84871633+jud21@users.noreply.github.com>
2021-10-18 09:36:56 -0500

    Add files via upload

---
+++ Desktop/Operators-73/week-1/carinputs.sql
@@ -0,0 +1,69 @@
+INSERT INTO mechanic(
+	mech_id,
+	first_name,
+	last_name,
+	phone,
+	email
+)VALUES(
+	'2',
+	'Henry',
+	'Dillon',
+	'3124519978',
+	'henintheden@yahoo.com'
+);
+
+INSERT INTO mechanic(
+	mech_id,
+	first_name,
+	last_name,
+	phone,
+	email
+)VALUES(
+	'1',
+	'Gunt',
+	'Myers',
+	'3124517878',
+	'gunt@myersmech.com'
+);
+
+INSERT INTO parts_inventory(
+	part_id,
+	part_name,
+	part_quantity
+)VALUES(
+	'2',
+	'Tire',
+	'44'
+);
+
+INSERT INTO parts_inventory(
+	part_id,
+	part_name,
+	part_quantity
+)VALUES(
+	'1',
+	'Gasket',
+	'600'
+);
+
+INSERT INTO car_inventory(
+	car_model,
+	car_make,
+	car_year,
+)VALUES(
+	'Escape',
+	'Ford',
+	2017
+);
+
+INSERT INTO car_inventory(
+	car_id,
+	car_model,
+	car_make,
+	car_year
+)VALUES(
+	'2',
+	'Porsche',
+	'Cayenne',
+	'2021'
+);
